03ed741bd46fa5259484c3cd343c847b033a8f3f
srikatre <srikatre@LIN73000766.corp.capgemini.com>
2020-11-30 13:19:15 +0530

    Starters added

---
+++ AttendanceManagementUsingSprint2/src/main/java/com/cg/amt/AttendanceManagementUsingSprint2Application.java
@@ -0,0 +1,13 @@
+package com.cg.amt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceManagementUsingSprint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceManagementUsingSprint2Application.class, args);
+	}
+
+}
---
+++ AttendanceManagementUsingSprint2/src/test/java/com/cg/amt/AttendanceManagementUsingSprint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.amt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementUsingSprint2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
